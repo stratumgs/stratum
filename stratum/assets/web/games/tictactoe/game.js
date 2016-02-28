471ff33760beed8dfb4cef3006823de3a0186388
Dave Korhumel <dpk2442@gmail.com>
2016-02-28 04:11:02 -0500

    fix TicTacToe web view javascript

--- stratum/assets/web/games/tictactoe/game.js
+++ stratum/assets/web/games/tictactoe/game.js
@@ -11,12 +11,13 @@
     socket_url += window.location.pathname + '/socket';
     var ws = new WebSocket(socket_url);
     ws.onmessage = function (msg) {
-        var state = JSON.parse(msg.data.substring(6)),
+        var obj = JSON.parse(msg.data),
+            state = JSON.parse(obj["payload"]),
             cells = document.querySelectorAll('#tictactoe-table td');
         var k = 0;
-        for (var i in state) {
-            for (var j in state[i]) {
-                cells[k].textContent = state[i][j];
+        for (var i in state["board"]) {
+            for (var j in state["board"][i]) {
+                cells[k].textContent = state["board"][i][j];
                 k++;
             }
         }
