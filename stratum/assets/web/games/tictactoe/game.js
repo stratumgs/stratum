a0dae3ca4b9b3dcbdb301db449ce8335aa2598b6
Dave Korhumel <dpk2442@gmail.com>
2016-02-29 19:38:42 -0500

    abstract view javascript logic to a common file

--- stratum/assets/web/games/tictactoe/game.js
+++ stratum/assets/web/games/tictactoe/game.js
@@ -1,20 +1,9 @@
-(function () {
+(function() {
     'use strict';
 
-    var socket_url;
-    if (window.location.protocol === 'https:') {
-        socket_url = 'wss:';
-    } else {
-        socket_url = 'ws:';
-    }
-    socket_url += '//' + window.location.host;
-    socket_url += window.location.pathname + '/socket';
-    var ws = new WebSocket(socket_url);
-    ws.onmessage = function (msg) {
-        var obj = JSON.parse(msg.data),
-            state = JSON.parse(obj["payload"]),
-            cells = document.querySelectorAll('#tictactoe-table td');
-        var k = 0;
+    StratumView.onstate = function(state) {
+        var cells = document.querySelectorAll('#tictactoe-table td'),
+            k = 0;
         for (var i in state["board"]) {
             for (var j in state["board"][i]) {
                 cells[k].textContent = state["board"][i][j];
