82851b03152f14ed6e820fdf34f6e703f7e8c091
Dave Korhumel <dpk2442@gmail.com>
2016-05-16 21:09:52 -0400

    add classifiers, an entry point, and update url

--- setup.py
+++ setup.py
@@ -12,8 +12,21 @@ setup(name='stratumgs',
       version=stratumgs.version,
       description='A turn based game engine designed to pit autonomous players against each other.',
       long_description=readme(),
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Environment :: Web Environment',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Topic :: Games/Entertainment :: Board Games',
+            'Topic :: Games/Entertainment :: Puzzle Games',
+            'Topic :: Games/Entertainment :: Turn Based Strategy'
+      ],
+      entry_points={
+            'console_scripts': ['stratumgs=stratumgs:main']
+      },
       keywords=['stratumgs'],
-      url='https://github.com/stratumgs/stratumgs',
+      url='https://stratumgs.org',
       author='David Korhumel',
       author_email='dpk2442@gmail.com',
       license='MIT',
